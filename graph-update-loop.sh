80d5f7eb46c50c9069ac3ccafdff48334ab4db68
Selene Scriven <git@toykeeper.net>
2018-02-17 00:37:37 -0700

    Only graph the past week worth of data.

--- graph-update-loop.sh
+++ graph-update-loop.sh
@@ -5,7 +5,7 @@ INFILE='fuckometer.log'
 while true ; do
   sleep 10
   if [ "$INFILE" -nt "$OUTFILE" ]; then
-    ./fuckometer-graph.py fuckometer.log
+    ./fuckometer-graph.py fuckometer.log $(expr 6 \* 24 \* 7)
     echo -n 'Graph update: ' ; date
   fi
 done
