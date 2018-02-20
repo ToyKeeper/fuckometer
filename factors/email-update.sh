a63e69bd080c89410d239f32d776c3d13c7805b4
Selene Scriven <git@toykeeper.net>
2018-02-19 17:45:20 -0700

    Gave email formulas a floor so we won't give a fuck about anything less than FLOOR messages in the queue.

--- factors/email-update.sh
+++ factors/email-update.sh
@@ -16,13 +16,13 @@ while true ; do
   (echo -n 'Mail: ' && grep TODO "$TMP") > "$BASE/email_todo/text"
   N=$(grep TODO "$TMP" | awk '{ print $1 }')
   echo "$N" > "$BASE/email_todo/raw"
-  python -c "import math ; print(math.sqrt($N) * 10)" > "$BASE/email_todo/fucks"
+  python -c "import math ; print(math.sqrt(max(0, $N - 5)) * 10)" > "$BASE/email_todo/fucks"
 
   # all email inboxes combined
   (echo -n 'Mail: ' && grep INBOX "$TMP") > "$BASE/email_inboxes/text"
   N=$(grep INBOX "$TMP" | awk '{ print $1 }')
   echo "$N" > "$BASE/email_inboxes/raw"
-  python -c "import math ; print(math.sqrt($N))" > "$BASE/email_inboxes/fucks"
+  python -c "import math ; print(math.sqrt(max(0, $N - 50)))" > "$BASE/email_inboxes/fucks"
   
   rm -f "$TMP"
   if [ "$ONCE" = 1 ]; then exit 0 ; fi
